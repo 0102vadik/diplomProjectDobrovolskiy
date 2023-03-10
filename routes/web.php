edf9d0d2b51eb25a5147f68c10f5a73e3a6dc619
Nikita Kotegov <nikita.kotegov@mail.ru>
2023-03-10 17:33:03 +0300

    updated routes
    - removed duplicate routes

--- routes/web.php
+++ routes/web.php
@@ -21,10 +21,6 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
 Route::get('/register/student', function (){
    return view('forms.student');
 })->name('register-student');
